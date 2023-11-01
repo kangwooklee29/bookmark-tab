aa2a8328a8e4f0d7a1bf2be4240a9da4f8ccd536
kangwooklee29 <kangwooklee29@gmail.com>
2023-11-01 20:18:30 +0900

    Update weather widget to work well (but it still has the too-often-update issue)

--- weather.js
+++ weather.js
@@ -1,68 +1,5 @@
 let API =  (navigator.userAgent.indexOf("Firefox") != -1) ? browser : chrome;
 
-function get_time_num() {
-    const nowDate = new Date();
-    const quotient = (nowDate.getHours() * 60 + nowDate.getMinutes() - 130);
-    let time_num = Math.floor((nowDate.getHours() * 60 + nowDate.getMinutes() - 130) / 180) * 180 + 130;
-    if (quotient < 0) {
-        time_num = 1390;
-    }
-    return time_num;
-}
-
-function get_lunar_day() {
-    const date1 = new Date();
-    const date2 = new Date('2021-09-06');
-    const interval = Math.floor((date1 - date2) / (1000 * 3600 * 24)) % 59;
-    return interval > 29 ? interval - 29 : interval;
-}
-
-function get_number_str(encoded_str) {
-    return /\d/.test(encoded_str) ? encoded_str : "-";
-}
-
-function day_icon_src(sky) {
-    if (sky <= 5) return "assets/sun.png";
-    if (sky <= 8) return "assets/sun_and_cloud.png";
-    return "assets/day_cloud.png";
-}
-
-function night_icon_src(sky) {
-    const lunar_day = get_lunar_day();
-    if (sky <= 5) return (lunar_day >= 12 && lunar_day <= 18) ? "assets/full_moon.png" : "assets/moon.png";
-    if (sky <= 8) return "assets/moon_and_cloud.png";
-    return "assets/night_cloud.png";
-}
-
-function sun_rises_at_6(nowDate) {
-    const start = new Date(nowDate.getFullYear(), 3, 13);
-    const end = new Date(nowDate.getFullYear(), 7, 30);
-    return (nowDate > start && nowDate < end);
-}
-
-function sun_rises_at_18(nowDate) {
-    const start = new Date(nowDate.getFullYear(), 1, 6);
-    const end = new Date(nowDate.getFullYear(), 9, 12);
-    return (nowDate > start && nowDate < end);
-}
-
-function get_icon_str(nowDate, time, sky, pty) {
-    const icon_src = {
-        "1": "assets/rain.png",
-        "2": "assets/rain_and_snow.png",
-        "3": "assets/snow.png",
-        "4": "assets/rain.png"
-    };
-    if (pty === "0") {
-        if (time <= 3 || time >= 21) return night_icon_src(sky);
-        if (time >= 9 && time <= 15) return day_icon_src(sky);
-        if (time === 6) return (sun_rises_at_6(nowDate)) ? day_icon_src(sky) : night_icon_src(sky);
-        return (sun_rises_at_18(nowDate)) ? day_icon_src(sky) : night_icon_src(sky);
-    } else {
-        return icon_src[pty];
-    }
-}
-
 async function run_weather(weatherInfo) {
     const table = document.getElementById('weatherTable');
 
@@ -94,11 +31,7 @@ async function run_weather(weatherInfo) {
       const row = document.createElement('tr');
       weatherInfo.forEach(info => {
         const cell = document.createElement('td');
-        if (key === 'icon') {
-          cell.innerHTML = `<img src="${info[key]}">`;
-        } else {
-          cell.innerHTML = info[key];
-        }
+        cell.innerHTML = info[key];
         row.appendChild(cell);
       });
       table.appendChild(row);
@@ -108,6 +41,6 @@ async function run_weather(weatherInfo) {
 
 API.storage.sync.get(['weather_info'], async (items) => {
   if (items.weather_info) {
-    run_weather(JSON.parse(items.weather_info));
+    run_weather(items.weather_info);
   }
 });
--- weather_background.js
+++ weather_background.js
@@ -7,6 +7,69 @@ fetchWeatherData();
 // Fetch weather data every hour
 setInterval(fetchWeatherData, 1000 * 60 * 60);
 
+function get_number_str(encoded_str) {
+  return /\d/.test(encoded_str) ? encoded_str : "-";
+}
+
+function get_time_num() {
+  const nowDate = new Date();
+  const quotient = (nowDate.getHours() * 60 + nowDate.getMinutes() - 130);
+  let time_num = Math.floor((nowDate.getHours() * 60 + nowDate.getMinutes() - 130) / 180) * 180 + 130;
+  if (quotient < 0) {
+      time_num = 1390;
+  }
+  return time_num;
+}
+
+function sun_rises_at_6(nowDate) {
+  const start = new Date(nowDate.getFullYear(), 3, 13);
+  const end = new Date(nowDate.getFullYear(), 7, 30);
+  return (nowDate > start && nowDate < end);
+}
+
+function sun_rises_at_18(nowDate) {
+  const start = new Date(nowDate.getFullYear(), 1, 6);
+  const end = new Date(nowDate.getFullYear(), 9, 12);
+  return (nowDate > start && nowDate < end);
+}
+
+function get_lunar_day() {
+  const date1 = new Date();
+  const date2 = new Date('2021-09-06');
+  const interval = Math.floor((date1 - date2) / (1000 * 3600 * 24)) % 59;
+  return interval > 29 ? interval - 29 : interval;
+}
+
+function day_icon_src(sky) {
+  if (sky <= 5) return "assets/sun.png";
+  if (sky <= 8) return "assets/sun_and_cloud.png";
+  return "assets/day_cloud.png";
+}
+
+function night_icon_src(sky) {
+  const lunar_day = get_lunar_day();
+  if (sky <= 5) return (lunar_day >= 12 && lunar_day <= 18) ? "assets/full_moon.png" : "assets/moon.png";
+  if (sky <= 8) return "assets/moon_and_cloud.png";
+  return "assets/night_cloud.png";
+}
+
+function get_icon_str(nowDate, time, sky, pty) {
+  const icon_src = {
+      "1": "assets/rain.png",
+      "2": "assets/rain_and_snow.png",
+      "3": "assets/snow.png",
+      "4": "assets/rain.png"
+  };
+  if (pty === "0") {
+      if (time <= 3 || time >= 21) return night_icon_src(sky);
+      if (time >= 9 && time <= 15) return day_icon_src(sky);
+      if (time === 6) return (sun_rises_at_6(nowDate)) ? day_icon_src(sky) : night_icon_src(sky);
+      return (sun_rises_at_18(nowDate)) ? day_icon_src(sky) : night_icon_src(sky);
+  } else {
+      return icon_src[pty];
+  }
+}
+
 async function update_weather(weather_api, n) {
     const nowDate = new Date();
     const time_num = get_time_num();
@@ -75,13 +138,15 @@ async function update_weather(weather_api, n) {
           pop: pop[i]
         });
       }
+
+      console.log("successfully updated weather info!");
     }
 
     API.storage.sync.set({weather_info: weather_info});
 }
 
 function fetchWeatherData() {
-    API.storage.sync.get(['weather_api'], async (items) => {
+    API.storage.sync.get(null, async (items) => {
         if (items.weather_api) {
             update_weather(items.weather_api, n);
         }

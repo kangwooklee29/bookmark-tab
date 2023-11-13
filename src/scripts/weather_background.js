a95498840fe3460d7ec124cb8333719646b35255
kangwooklee29 <kangwooklee29@gmail.com>
2023-11-13 19:02:02 +0900

    Address time update issue when diff between store_date & cur_date > 1h

--- src/scripts/weather_background.js
+++ src/scripts/weather_background.js
@@ -124,7 +124,7 @@ async function update_weather(cur_date, stored_date, weather_loc, prev_weather_i
     // cur_date가 19시인데 stored_date가 18, 21, 0, 3, 6시면 업데이트 해야.
     // cur_date가 20시인데 stored_date가 19, 21, 0, 3, 6, 9시면 업데이트 x.
     // cur_date가 21시인데 storde_date가 20, 21, 0, 3, 6, 9시면 업데이트 x.
-    if (cur_date.getHours() % 3 == 0 || !prev_weather_info) {
+    if (cur_date.getHours() % 3 == 0 || !prev_weather_info || cur_date - stored_date >= 3600000) {
       url = `https://api.openweathermap.org/data/2.5/forecast?${params.toString()}`;
       res_json = null;
       for (let i = 0; i < 5; i++) {
